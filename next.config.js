765a6bbac355bf0d035bcdc0f970e2bed589a268
kanak-aurasoft <kanak.aurasoft@gmail.com>
2025-12-09 16:53:43 +0530

    Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     transpilePackages: ['lucide-react'], // Sometimes needed for icons
     images: {
